db327fde7fc61c15a692314f0641bcf9a89c4ae7
jhonvincenthernandez <jhonvincenthernandez9@gamil.com>
2025-09-19 14:47:02 +0800

    commit

--- app/controllers/CrudController.php
+++ app/controllers/CrudController.php
@@ -13,8 +13,32 @@ class CrudController extends Controller {
     }
     public function index()
     {
-        $data = $this->UserModel->All();
-        $this->call->view('View', $data);
+        $page = 1;
+        if(isset($_GET['page']) && ! empty($_GET['page'])) {
+            $page = $this->io->get('page');
+        }
+
+        $q = '';
+        if(isset($_GET['q']) && ! empty($_GET['q'])) {
+            $q = trim($this->io->get('q'));
+        }
+
+        $records_per_page = 10;
+
+        $all = $this->UserModel->page($q, $records_per_page, $page);
+        $data['all'] = $all['records'];
+        $total_rows = $all['total_rows'];
+        $this->pagination->set_options([
+            'first_link'     => '⏮ First',
+            'last_link'      => 'Last ⏭',
+            'next_link'      => 'Next →',
+            'prev_link'      => '← Prev',
+            'page_delimiter' => '&page='
+        ]);
+        $this->pagination->set_theme('bootstrap'); // or 'tailwind', or 'custom'
+        $this->pagination->initialize($total_rows, $records_per_page, $page, site_url('author').'?q='.$q);
+        $data['page'] = $this->pagination->paginate();
+        $this->call->view('authors', $data);
     }
 
 
