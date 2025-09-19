547269d3b24652871571ef9b3e7444d4101297a9
jhonvincenthernandez <jhonvincenthernandez9@gamil.com>
2025-09-19 19:39:05 +0800

    commit

--- app/views/View.php
+++ app/views/View.php
@@ -6,6 +6,8 @@
     <title>User List</title>
     <link rel="icon"  href="<?=base_url().'public/icon.png';?>">
     <link rel="stylesheet" href="<?=base_url().'public/style.css';?>"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
   <div class="container mt-3 ">
