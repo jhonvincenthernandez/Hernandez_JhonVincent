21c7b941780a7373a4e7aa5b9874f220354a5d8b
jhonvincenthernandez <jhonvincenthernandez9@gamil.com>
2025-09-19 18:52:39 +0800

    commit

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798363',
-    'password'	=> 'psKFa8NHQP',
-    'database'	=> 'sql12798363',
+    'username'	=> 'sql12799254',
+    'password'	=> ' dqPs5yA298',
+    'database'	=> 'sql12799254',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
