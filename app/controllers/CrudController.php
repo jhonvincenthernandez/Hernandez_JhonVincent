d5cc3f78e6762e5e85f2d0b3b4ad6e598889d3b8
jhonvincenthernandez <jhonvincenthernandez9@gamil.com>
2025-09-19 15:41:38 +0800

    aaa

--- app/controllers/CrudController.php
+++ app/controllers/CrudController.php
@@ -26,7 +26,7 @@ class CrudController extends Controller {
         $records_per_page = 10;
 
         $all = $this->UserModel->page($q, $records_per_page, $page);
-        $data['all'] = $all['records'];
+        $data['data'] = $all['records'];
         $total_rows = $all['total_rows'];
         $this->pagination->set_options([
             'first_link'     => '⏮ First',
